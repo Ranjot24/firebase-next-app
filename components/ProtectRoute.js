c9da1707b34a90b23a250059694363750e2d6904
Ranjot Singh <61102985+Ranjot24@users.noreply.github.com>
2025-01-24 11:32:56 +0530

    Create ProtectRoute.js

---
+++ components/ProtectRoute.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+
+export default function ProtectRoute({ children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push("/login");
+      }
+    });
+    return () => unsubscribe();
+  }, [router]);
+
+  return <>{children}</>;
+}
